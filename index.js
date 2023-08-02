3aaefbf98704e247ca3f7bebe7377b1525897892
kelvin handoko <kelvinhandoko03@gmail.com>
2023-08-02 22:47:14 +0700

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+;```
+FizzBuzz
+
+jika n dibagi habis 3 -> maka kembalikan 'Fizz'
+jika n dibagi habis 5 -> maka kembalikan 'Buzz'
+jika n dibagi habis 15 -> maka kembalikan 'FizzBuzz'
+jika tidak memenuhi 3 syarat diatas -> maka kembalikan angka tersebut.
+```
+
+function FizzBuzz(n) {
+    // lanjutkan code disini
+}
+
+// ini jangan di edit
+// jika sudah selesai di code, di terminal ketik npm run test
+module.exports = FizzBuzz
